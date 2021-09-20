d1abc1f84168420d0343db0ce9aa46971ca1af17
BATON <v.sadriev@g.nsu.ru>
2021-09-20 17:19:40 +0700

    make some methods

--- flatmap/main.cpp
+++ flatmap/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <cstring>
 
 using std::size_t;
 using std::cout;
@@ -19,7 +20,7 @@ public:
 	void push_back(T elem);
 	void erase(std::size_t idx);
 
-	Array<T> &operator = (const Array<T> &orig);
+	Array<T> &operator=(const Array<T> &orig);
 
 private:
 	T *data_ = nullptr;
@@ -135,16 +136,16 @@ template<class Key, class Value>
 class FlatMap
 {
 public:
-	FlatMap();
+	explicit FlatMap(size_t size = 1);
 	~FlatMap();
 
 	FlatMap(const FlatMap<Key, Value> &other);
-	FlatMap(FlatMap &&other);
+	//FlatMap(FlatMap &&other);
 
 	void swap(FlatMap<Key, Value> &other);
 
-	FlatMap<Key, Value> &operator=(const FlatMap<Key, Value>);
-	FlatMap<Key, Value> &&operator=(FlatMap<Key, Value> &&other);
+	FlatMap<Key, Value> &operator=(const FlatMap<Key, Value> &other);
+	//FlatMap<Key, Value> &&operator=(FlatMap<Key, Value> &&other);
 
 	void clear();
 	bool erase(const Key &key);
@@ -155,20 +156,50 @@ public:
 	Value &operator[](const Key &key);
 
 	Value &at(const Key &key);
-
 	const Value &at(const Key &key) const;
 
-	size_t size() const; //get size of container
+	size_t size() const; //get size of the container
 	bool empty() const; //is container empty
 
 	friend bool operator ==(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second);
 	friend bool operator !=(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second);
 
 private:
-	Array<Value> arr_;
+	Array<Key> key_arr_;
+	Array<Value> val_arr_;
 	size_t size_ = 0;
 	size_t load_ = 0;
+
+	bool bin_search(const Key &key, size_t &idx); //??????????????
+
+	void make_shift_left(size_t idx);
+	void make_shift_right(size_t idx);
+
+
 };
+
+template<class Key, class Value>
+void FlatMap<Key, Value>::make_shift_left(size_t idx)
+{
+	assert(idx >= 0);
+	for (size_t i = idx; i < load_ - 1; ++i)
+	{
+		key_arr_[i] = key_arr_[i + 1];
+		val_arr_[i] = val_arr_[i + 1];
+	}
+}
+
+template<class Key, class Value>
+void FlatMap<Key, Value>::make_shift_right(size_t idx)
+					//[idx + 1] = [idx], not [idx] = [idx - 1]
+{
+	assert(idx >= 0);
+	for (size_t i = load_ - 1; i > idx; --i)
+	{
+		key_arr_[i] = key_arr_[i - 1];
+		val_arr_[i] = val_arr_[i - 1];
+	}
+}
 
 template<class Key, class Value>
 bool FlatMap<Key, Value>::empty() const
@@ -181,6 +212,110 @@ size_t FlatMap<Key, Value>::size() const
 {
 	return size_;
 }
+
+template<class Key, class Value>
+bool FlatMap<Key, Value>::bin_search(const Key &key, size_t &idx)
+{
+	size_t left = 0;
+	size_t right = load_ - 1;
+	while (left <= right)
+	{
+		size_t mid = right / 2 + left / 2;
+		if (key_arr_[mid] == key)
+		{
+			idx = mid;
+			return true;
+		}
+		if (key < key_arr_[mid])
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	idx = load_; //?????????????
+	return false;
+}
+
+template<class Key, class Value>
+FlatMap<Key, Value>::FlatMap(size_t size) : size_{size}
+{
+	key_arr_ = new Key[size];
+	val_arr_ = new Value[size];
+}
+
+template<class Key, class Value>
+FlatMap<Key, Value>::~FlatMap()
+{
+	delete[] key_arr_;
+	delete[] val_arr_;
+}
+
+template<class Key, class Value>
+FlatMap<Key, Value>::FlatMap(const FlatMap<Key, Value> &other) :size_{other.size_}
+{
+	key_arr_ = new Key[other.size_];
+	val_arr_ = new Value[other.size_];
+	for (size_t i = 0; i < other.size; ++i)
+	{
+		key_arr_[i] = other.key_arr_[i];
+		val_arr_[i] = other.val_arr_[i];
+	}
+}
+
+template<class Key, class Value>
+FlatMap<Key, Value> &FlatMap<Key, Value>::operator=(const FlatMap<Key, Value> &other)
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+
+	delete[] key_arr_;
+	delete[]val_arr_;
+	size_ = other.size_;
+	key_arr_ = new Key[other.size_];
+	val_arr_ = new Value[other.size_];
+
+	for (size_t i = 0; i < other.size_; ++i)
+	{
+		key_arr_[i] = other.key_arr_[i];
+		val_arr_[i] = other.val_arr_[i];
+	}
+	return *this;
+}
+
+template<class Key, class Value>
+void FlatMap<Key, Value>::clear()
+{
+	memset(&key_arr_, 0, sizeof(Key) * load_); //???????? todo
+	memset(&val_arr_, 0, sizeof(Value) * load_);
+	size_ = 0;
+	load_ = 0;
+}
+
+template<class Key, class Value>
+bool FlatMap<Key, Value>::contains(const Key &key) const
+{
+	size_t idx = 0;
+	return bin_search(key, idx);
+}
+
+template<class Key, class Value>
+bool FlatMap<Key, Value>::erase(const Key &key)
+{
+	size_t idx = 0;
+	if (bin_search(key, idx))
+	{
+		make_shift_left(idx);
+		return true;
+	}
+	return false;
+}
+
+
 
 
 int main()
