8cd91d26bb55e3ffa45e0c059b4a3dceeeb739d0
BATON <v.sadriev@g.nsu.ru>
2021-09-20 22:30:56 +0700

    making methods for myArray

--- flatmap/main.cpp
+++ flatmap/main.cpp
@@ -20,11 +20,14 @@ public:
 	void push_back(T elem);
 	void erase(std::size_t idx);
 
-	Array<T> &operator=(const Array<T> &orig);
+	Array<T> &operator =(const Array<T> &orig);
+	Array<T> &operator [](const size_t idx);
+	const Array<T> &operator [](const size_t idx) const;
+
 
 private:
 	T *data_ = nullptr;
-	std::size_t size_ = 0;
+	std::size_t load_ = 0;
 	std::size_t capacity_ = 0;
 };
 
@@ -36,10 +39,10 @@ Array<T>::Array(std::size_t size) : capacity_ {size}
 }
 
 template<class T>
-Array<T>::Array(const Array<T> &orig) : size_ {orig.size_}, capacity_ {orig.capacity_}
+Array<T>::Array(const Array<T> &orig) : load_ {orig.load_}, capacity_ {orig.capacity_}
 {
 	data_ = new T[orig.capacity_];
-	for (std::size_t i = 0; i < orig.size_; ++i)
+	for (std::size_t i = 0; i < orig.load_; ++i)
 	{
 		data_[i] = orig.data_[i];
 	}
@@ -78,40 +81,40 @@ T Array<T>::get_by_idx(std::size_t idx) const
 template<class T>
 std::size_t Array<T>::get_size() const
 {
-	return size_;
+	return capacity_;
 }
 
 template<class T>
 void Array<T>::push_back(T elem)
 {
-	if (size_ == capacity_)
+	if (load_ == capacity_)
 	{
 		data_ = resize(capacity_ * 2);
 	}
-	data_[size_++] = elem;
+	data_[load_++] = elem;
 }
 
 template<class T>
 void Array<T>::erase(std::size_t idx)
 {
 	assert(idx < capacity_);
-	--size_;
-	for (std::size_t i = 0; i < size_; ++i)
+	--load_;
+	for (std::size_t i = 0; i < load_; ++i)
 	{
 		data_[i] = data_[i + 1];
 	}
 }
 
 template<class T>
-Array<T> &Array<T>::operator =(const Array<T> &orig)
+Array<T> &Array<T>::operator=(const Array<T> &orig)
 {
 	if (&orig != this)  //checking for self-assignment
 	{
 		delete[] data_;
-		size_ = orig.size_;
+		load_ = orig.load_;
 		data_ = new T[orig.capacity_];
 
-		for (std::size_t i = 0; i < orig.size_; ++i)
+		for (std::size_t i = 0; i < orig.load_; ++i)
 		{
 			data_[i] = orig.data_[i];
 		}
@@ -120,6 +123,18 @@ Array<T> &Array<T>::operator =(const Array<T> &orig)
 	}
 
 	return *this;
+}
+
+template<class T>
+Array<T> &Array<T>::operator [](const size_t idx)
+{
+	return data_[idx];
+}
+
+template<class T>
+const Array<T> &Array<T>::operator [](const size_t idx) const
+{
+	return data_[idx];
 }
 
 void print_arr(const Array<int> &arr)
@@ -150,7 +165,6 @@ public:
 	void clear();
 	bool erase(const Key &key);
 	bool insert(const Key &key, const Value &value);
-
 	bool contains(const Key &key) const;
 
 	Value &operator[](const Key &key);
@@ -161,21 +175,48 @@ public:
 	size_t size() const; //get size of the container
 	bool empty() const; //is container empty
 
-	friend bool operator ==(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second);
-	friend bool operator !=(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second);
+	friend bool operator ==(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second)
+	{
+		if (first.load_ != second.load_)
+		{
+			return false;
+		}
+		for (size_t i = 0; i < first.load_; ++i)
+		{
+			if (first.key_arr_[i] != second.key_arr_[i] || first.val_arr_[i] != second.val_arr_[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	friend bool operator !=(const FlatMap<Key, Value> &first, const FlatMap<Key, Value> &second)
+	{
+		if (first.load_ == second.load_)
+		{
+			return true;
+		}
+		for (size_t i = 0; i < first.load_; ++i)
+		{
+			if (first.key_arr_[i] == second.key_arr_[i] || first.val_arr_[i] == second.val_arr_[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
 private:
-	Array<Key> key_arr_;
-	Array<Value> val_arr_;
+	Array<Key> *key_arr_;
+	Array<Value> *val_arr_;
 	size_t size_ = 0;
 	size_t load_ = 0;
 
-	bool bin_search(const Key &key, size_t &idx); //??????????????
+	size_t bin_search(const Key &key);
 
 	void make_shift_left(size_t idx);
 	void make_shift_right(size_t idx);
-
-
+	void resize(size_t new_size);
 };
 
 template<class Key, class Value>
@@ -214,7 +255,7 @@ size_t FlatMap<Key, Value>::size() const
 }
 
 template<class Key, class Value>
-bool FlatMap<Key, Value>::bin_search(const Key &key, size_t &idx)
+size_t FlatMap<Key, Value>::bin_search(const Key &key)
 {
 	size_t left = 0;
 	size_t right = load_ - 1;
@@ -223,8 +264,11 @@ bool FlatMap<Key, Value>::bin_search(const Key &key, size_t &idx)
 		size_t mid = right / 2 + left / 2;
 		if (key_arr_[mid] == key)
 		{
-			idx = mid;
-			return true;
+			return mid;
+		}
+		if (left == right)
+		{
+			return left + 1;
 		}
 		if (key < key_arr_[mid])
 		{
@@ -235,15 +279,13 @@ bool FlatMap<Key, Value>::bin_search(const Key &key, size_t &idx)
 			left = mid + 1;
 		}
 	}
-	idx = load_; //?????????????
-	return false;
 }
 
 template<class Key, class Value>
 FlatMap<Key, Value>::FlatMap(size_t size) : size_{size}
 {
-	key_arr_ = new Key[size];
-	val_arr_ = new Value[size];
+	key_arr_ = new Array<Key>(size);
+	val_arr_ = new Array<Value>(size);
 }
 
 template<class Key, class Value>
@@ -299,15 +341,15 @@ void FlatMap<Key, Value>::clear()
 template<class Key, class Value>
 bool FlatMap<Key, Value>::contains(const Key &key) const
 {
-	size_t idx = 0;
-	return bin_search(key, idx);
+	size_t idx = bin_search(key);
+	return (key == key_arr_[idx]);
 }
 
 template<class Key, class Value>
 bool FlatMap<Key, Value>::erase(const Key &key)
 {
-	size_t idx = 0;
-	if (bin_search(key, idx))
+	size_t idx = bin_search(key);
+	if (key == key_arr_[idx])
 	{
 		make_shift_left(idx);
 		return true;
@@ -315,8 +357,36 @@ bool FlatMap<Key, Value>::erase(const Key &key)
 	return false;
 }
 
+template<class Key, class Value>
+bool FlatMap<Key, Value>::insert(const Key &key, const Value &value)
+{
+	if (++load_ == size_)
+	{
+		resize(size_ * 2);
+	}
+	size_t idx = bin_search(key);
+	make_shift_right(idx);
+	key_arr_[idx] = key;
+	val_arr_[idx] = value;
+	return true;
+}
 
-
+template<class Key, class Value>
+void FlatMap<Key, Value>::resize(size_t new_size)
+{
+	Array<Key> *tmp_key = new Key[new_size];
+	Array<Value> *tmp_val = new Value[new_size];
+	for (size_t i = 0; i < size_; ++i)
+	{
+		tmp_key[i] = key_arr_[i];
+		tmp_val[i] = val_arr_[i];
+	}
+	size_ = new_size;
+	delete[] key_arr_;
+	delete[] val_arr_;
+	key_arr_ = tmp_key;
+	val_arr_ = tmp_val;
+}
 
 int main()
 {
